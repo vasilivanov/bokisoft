78248d5282401b766217c31efec706a635228b7c
Vasil Ivanov <shviizbiq@gmail.com>
2022-01-12 17:30:13 +0200

    radio buttons test

--- ng9-clr3/src/app/clarity-test/clarity-test.module.ts
+++ ng9-clr3/src/app/clarity-test/clarity-test.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StackViewTestComponent } from './stack-view-test/stack-view-test.component';
-import { ClarityModule } from "@clr/angular";
+import { ClarityModule, ClrFormsModule } from "@clr/angular";
 import { ReusableFormsModule } from "../reusable-forms/reusable-forms.module";
 import { CustomAccordionPanelComponent } from './custom-accordion-panel/custom-accordion-panel.component';
 import { ClarityTestComponent } from './clarity-test/clarity-test.component';
 import { PipesModule } from '../pipes/pipes.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 
@@ -17,6 +18,8 @@ import { PipesModule } from '../pipes/pipes.module';
    imports: [
       CommonModule,
       ClarityModule,
+     ReactiveFormsModule,
+     ClrFormsModule,
       ReusableFormsModule,
       PipesModule
    ]
--- ng9-clr3/src/app/clarity-test/clarity-test/clarity-test.component.ts
+++ ng9-clr3/src/app/clarity-test/clarity-test/clarity-test.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, of, throwError } from 'rxjs';
+import { FormControl, FormGroup } from '@angular/forms';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-clarity-test',
@@ -8,6 +11,8 @@ import { Component, OnInit } from '@angular/core';
 export class ClarityTestComponent implements OnInit {
 
   constructor() { }
+
+  positionForm = new FormGroup({"position": new FormControl('')});
 
   itemToFind: any;
 
@@ -26,11 +31,32 @@ export class ClarityTestComponent implements OnInit {
         return;
       }
     }
+
+    this.positionForm.get('position').valueChanges.subscribe((value: string) => {
+
+    });
   }
 
   toMegabytes(valueInBytes: any) {
     console.log('toMegabytes function transforming');
     return (valueInBytes / (1024*1024)).toFixed(2).toString() + ' MB';
   }
+
+  validate() {
+    console.log('Validate');
+    of(false).pipe(
+      switchMap(() => this.doSomething(null))
+    ).subscribe((result) => { console.log('Aaand the result is...' + result)})
+  }
+
+  doSomething(vmId: string | null): Observable<boolean> {
+    if (!vmId) {
+      return throwError(new Error('vmId is required'));
+      //throw new Error('vmId is required');
+    } else {
+      return of(true);
+    }
+  }
+
 
 }
